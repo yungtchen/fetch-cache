5d7a82bd5a8e21d9dadeeb4b2d340281b4fd04c7
yung <yungtchen@gmail.com>
2021-01-21 21:47:37 -0500

    Feature: Setup additional tests

---
+++ tests/integration/api-test.js
@@ -0,0 +1,10 @@
+describe('fetch-cache http methods', () => {
+  it('get successfully', () => {});
+  it('post successfully', () => {});
+  it('put successfully', () => {});
+  it('delete successfully', () => {});
+});
+
+describe('fetch-cache cache hits', () => {
+  it('get successfully cached', () => {});
+});
