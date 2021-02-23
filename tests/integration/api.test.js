d25fe3c77376d449721fde8396ee48ff242e8d0d
yung <yungtchen@gmail.com>
2021-02-22 22:59:44 -0500

    Feature: Setup jest tests

---
+++ tests/integration/api.test.js
@@ -0,0 +1,20 @@
+import FetchCache from '../../src/index';
+// using the global isomorphic-fetch for browser and node support
+require('isomorphic-fetch');
+
+describe('fetch-cache http methods', () => {
+  it('get successfully', async () => {
+    const fc = new FetchCache();
+    const url = 'https://reqres.in/api/users?page=2';
+    const result = await fc.get(url);
+    console.log(result);
+    expect(result);
+  });
+  it('post successfully', () => {});
+  it('put successfully', () => {});
+  it('delete successfully', () => {});
+});
+
+describe('fetch-cache cache hits', () => {
+  it('get successfully cached', () => {});
+});
